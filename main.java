08da697dea123974b2f660eac74f3737a9972881
sumeetk321 <sumeetk321@yahoo.com>
2016-04-22 16:31:34 -0700

    Update main.java

--- main.java
+++ main.java
@@ -3,5 +3,23 @@
 import java.io.*;
 import java.util.*;
 import javax.script;
+import javax.swing.*;
 
-//Hopefully this works
+public class Main 
+{
+  public static void main (String[] args)
+  {
+    public int StartBullets = 200; //Let me know if 200 bullets is too much, too little, etc.
+    public static void SubtractBullets
+    {
+      StartBullets = StartBullets - 1;
+    }
+    public static void ShootGun
+    {
+      if (MouseEvent e) //I'm not sure if this will work, haven't done much research into mouseclick functions
+      {
+        Main.SubtractBullets;
+      }
+    }
+  }
+}
